2243877ce993dba572b11d94fc77273943f435e9
Cibbi546 <105946907+Cibbi546@users.noreply.github.com>
2023-07-07 11:15:24 +0530

    Update Install-jenkins.sh

--- Install-jenkins.sh
+++ Install-jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf install java-17-amazon-corretto-devel
+sudo dnf -y install java-17-amazon-corretto-devel
 java -version
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
